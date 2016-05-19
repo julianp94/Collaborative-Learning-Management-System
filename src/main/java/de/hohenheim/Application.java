d7a019d8edeb27e786dcaab295ebdfbd1aa18e1c
julianp94 <julian.prokosch@web.de>
2016-05-19 17:01:59 +0200

    Getting started
    Start des Projekts, Website mit "Hallo, User" läuft

---
+++ src/main/java/de/hohenheim/Application.java
@@ -0,0 +1,16 @@
+package de.hohenheim;/**
+ * Created by Julian on 18.05.2016.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
