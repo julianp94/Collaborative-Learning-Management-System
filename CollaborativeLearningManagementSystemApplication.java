d0f638925d00b1737701281c569168822af51983
ziadmaraghi <ziad.maraghi@hotmail.com>
2016-05-23 19:19:01 +0200

    Add files via upload

---
+++ CollaborativeLearningManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package de.hohenheim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//hier wird application gebootet
+
+@SpringBootApplication
+public class CollaborativeLearningManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollaborativeLearningManagementSystemApplication.class, args);
+	}
+}
