5fd7bf55d93d8656d340568de4e212505631fbf5
bnymnbgn <st4b@hotmail.com>
2016-06-08 22:14:01 +0200

    Add files via upload

---
+++ src/main/java/de/hohenheim/UserController.java
@@ -0,0 +1,34 @@
+package de.hohenheim.controller;
+
+import de.hohenheim.form.LoginForm;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by bunyaminbilgin on 08.06.16.
+ */
+//Diese Klasse ist für die Einloggprozesse zuständig.
+@Controller
+//Unter diesen URL kann man die Page erreichen
+@RequestMapping("/")
+public class UserController {
+    //Wenn ich auf URL/login gehe:
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    //Hier kommt alles rein, was der Seite als Model (Formulare, Date, Übersetzungen... ) mitgeben soll
+    public String getLoginPage(ModelMap modelMap) {
+        modelMap.addAttribute("login", new LoginForm());
+        //Diese Seite soll im Template "login" heissen.
+        return "login";
+    }
+        @RequestMapping(value = "/login", method = RequestMethod.POST)
+        public String postLoginPage(@ModelAttribute(value = "login") LoginForm loginForm,  HttpServletRequest request){
+            //Soll nach Startseite redirektieren.
+            return "redirect:/";
+
+    }
+}
